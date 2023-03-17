72b34ccd6e4d1386c4bb36fe632f19c7aa73809a
Prameesh <prameepramee0@gmail.com>
2023-03-17 06:44:31 +0530

    started server side main.go

--- server/main.go
+++ server/main.go
@@ -1,1 +1,33 @@
 package server
+
+import (
+	"log"
+	"net"
+	pb "github.com/Prameesh-P/grpc-sample/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+type helloServer struct{
+
+	pb.GrpcServiceServer
+
+}
+
+func main() {
+	lis, err := net.Listen("tcp",port)
+
+	if err != nil{
+		log.Fatalf("failed Start the server..!! Got an error %v",err)
+	}
+
+	grpcServer:= grpc.NewServer()
+	pb.RegisterGrpcServiceServer(grpcServer,&helloServer{})
+	log.Printf("Server is started on %v",lis.Addr())
+	if err := grpcServer.Serve(lis);err!=nil{
+		log.Fatalf("Failed to start grpc %v",err)
+	}
+
+}
